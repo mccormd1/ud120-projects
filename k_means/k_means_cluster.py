d24b45354d853d3fa8bac71606d2dce7fa3a8569
Devin McCormack <devin.mccormack@gmail.com>
2017-05-17 08:24:54 -0500

    k means cluster completion
    completed k means clustering mini project and made it work for python 3

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -11,6 +11,7 @@ import pickle
 import numpy
 import matplotlib.pyplot as plt
 import sys
+from sklearn.cluster import KMeans
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -39,7 +40,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
@@ -48,29 +49,39 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
 features_list = [poi, feature_1, feature_2]
+#features_list = [poi, feature_1, feature_2, feature_3]
+
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+#print(max(data[data[:,1] != 0,1])) #max and min of a column
+#print(min(data[data[:,1] != 0,1]))
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
+
 for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
+# for f1, f2, f3 in finance_features:
+#     plt.scatter( f1, f2 )
+# plt.show()
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+cluster=KMeans(n_clusters=2,).fit(finance_features)
 
-
+pred=cluster.labels_
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+    print ("no predictions object named pred found, no clusters to plot")
